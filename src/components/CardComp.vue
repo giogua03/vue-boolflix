f1ef99aed29c7c92fad23416b6ef3cc0d2412ce4
giogua03 <giorgiogua03@gmail.com>
2022-12-02 17:03:05 +0100

    meta

---
+++ src/components/CardComp.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul >
+      <li><img src="imgUrl" :alt="title"></li>
+      <li>{{title}}</li>
+      <li>{{originalTitle}}</li>
+      <li><lang-flag iso="objMovie.original_language"/></li>
+      <li>{{score}}</li>  
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+
+import LangFlag from 'vue-lang-code-flags';
+export default {
+    name:'CardComp',
+    components:{
+      LangFlag
+    },
+    props:{
+      title:String,
+      originalTitle:String,
+      language:String,
+      score:Number,
+      imgUrl:String,
+
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,26 +1,36 @@
 <template>
   <div>
     <h2>FILM</h2>
-    <ul v-for="objMovie in arrMovies" :key="objMovie.id">
-      <li>{{objMovie.title}}</li>
-      <li>{{objMovie.original_title}}</li>
-      <li>{{objMovie.original_language}}</li>
-      <li>{{objMovie.vote_average}}</li>  
-    </ul>
+    <CardComp
+    v-for="objMovie in arrMovies"
+    :key="objMovie.id"
+      :title="objMovie.title"
+      :originalTitle="objMovie.original_title"
+      :language="objMovie.original_language"
+      :score="objMovie.vote_average"
+      :imgUrl="'https://image.tmdb.org/t/p/w342' + objMovie.poster_path"
+      />
+    
     <h2>SERIE TV</h2>
-    <ul v-for="objTv in arrTv" :key="objTv.id">
-      <li>{{objTv.name}}</li>
-      <li>{{objTv.original_name}}</li>
-      <li>{{objTv.original_language}}</li>
-      <li>{{objTv.vote_average}}</li>  
-    </ul>
+    <CardComp
+    v-for="objTv in arrTv"
+    :key="objTv.id"
+      :title="objTv.name"
+      :originalTitle="objTv.original_name"
+      :language="objTv.original_language"
+      :score="objTv.vote_average"
+      :imgUrl="'https://image.tmdb.org/t/p/w342' + objTv.poster_path"
+      />
   </div>
 </template>
 
 <script>
-
+import CardComp from '@/components/CardComp.vue'
 export default {
     name: 'PageMain',
+    components: {
+      CardComp
+    },
     props:{
       arrMovies: Array,
       arrTv: Array,
